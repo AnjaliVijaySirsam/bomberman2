387c7f62dff24506c0405d46be5da4f500a0a222
Anjali Sirsam <69607724+AnjaliVijaySirsam@users.noreply.github.com>
2022-02-02 23:11:42 +0530

    js updated

--- bomberman2/indexJavascript.js
+++ bomberman2/indexJavascript.js
@@ -1,13 +1,46 @@
-function handleOnClick() {
-        console.log("Left clicked");
-        let element = document.getElementById("1");
-        element.classList.add("checked");
-        if (element.innerHTML == "!") {
-          console.log("YOU LOSE!");
-          return;
+// function handleOnClick() {
+//         console.log("Left clicked");
+//         let element = document.getElementById("1");
+//         element.classList.add("checked");
+//         if (element.innerHTML == "!") {
+//           console.log("YOU LOSE!");
+//           return;
+//         }
+//       }
+//       function handleRightClick() {
+//         console.log("Right clicked");
+//         document.getElementById("1").innerHTML = "!";
+//       }
+
+//("")
+//onclick="handleOnClick()"
+
+const parentBox = document.getElementsByClassName("container")[0];
+ let show = document.getElementById("result");
+
+parentBox.addEventListener("click", (event) => {
+    const childBox = event.target;
+//    console.log(childBox);
+    childBox.classList.add("checked");
+    
+//      function handleRightClick() {
+////        console.log("Right clicked");
+////        document.getElementById("1").innerHTML = "!";
+//          childBox.innerHTML =  "!";
+//          childBox.classList.add("flag");
+//      }
+//     
+    
+    if(childBox.classList.contains("bomb"))
+        {
+           
+            show.innerHTML = "YOU LOSE...";
+            return;
         }
-      }
-      function handleRightClick() {
-        console.log("Right clicked");
-        document.getElementById("1").innerHTML = "!";
-      }
+    else
+    {
+//        childBox.getAttribute("data").value;
+        
+    }
+    
+})
