1c13bd139df639637a53eb290a318127eadb49f0
Anjali Sirsam <69607724+AnjaliVijaySirsam@users.noreply.github.com>
2022-02-03 19:32:41 +0530

    Update indexJavascript.js

--- bomberman2/indexJavascript.js
+++ bomberman2/indexJavascript.js
@@ -1,46 +1,65 @@
-// function handleOnClick() {
-//         console.log("Left clicked");
-//         let element = document.getElementById("1");
-//         element.classList.add("checked");
-//         if (element.innerHTML == "!") {
-//           console.log("YOU LOSE!");
-//           return;
-//         }
-//       }
-//       function handleRightClick() {
-//         console.log("Right clicked");
-//         document.getElementById("1").innerHTML = "!";
-//       }
+
 
 //("")
-//onclick="handleOnClick()"
-
+var flagLeft = 10;
+var score = 0;
 const parentBox = document.getElementsByClassName("container")[0];
- let show = document.getElementById("result");
+let resultElement = document.getElementById("result");
+let scoreElement = document.getElementById("score");
+let flagElement = document.getElementById("flagsLeft");
 
-parentBox.addEventListener("click", (event) => {
-    const childBox = event.target;
-//    console.log(childBox);
-    childBox.classList.add("checked");
-    
-//      function handleRightClick() {
-////        console.log("Right clicked");
-////        document.getElementById("1").innerHTML = "!";
-//          childBox.innerHTML =  "!";
-//          childBox.classList.add("flag");
-//      }
-//     
-    
-    if(childBox.classList.contains("bomb"))
-        {
-           
-            show.innerHTML = "YOU LOSE...";
-            return;
-        }
+
+//+++++++++++MOUSE -> RIGHT CLICK("BomFlagged")+++++++++++++
+
+parentBox.addEventListener("contextmenu", funct1= (e) => {
+  const childBox = e.target;
+  if (childBox.classList.contains("bomb")) {
+    // a bomb
+       
+        childBox.innerHTML = "!";
+            score++;
+         scoreElement.innerHTML = score;
+            flagLeft--;
+        flagElement.innerHTML = flagLeft;
+      }
+        
+  }
+});
+
+
+//+++++++++++MOUSE -> LEFT CLICK+++++++++++++
+
+parentBox.addEventListener("click",  funct2 = (event) => {
+  const childBox = event.target;
+  childBox.classList.add("checked");
+
+  
+  if (childBox.classList.contains("bomb")) {
+    // a bomb
+        resultElement.innerHTML = "YOU LOSE!";
+        parentBox.removeEventListener("contextmenu",funct1);
+        parentBox.removeEventListener("click",funct2);
+        return;
+    }
     else
     {
-//        childBox.getAttribute("data").value;
-        
-    }
+    // not a bomb
+        let cell = document.getElementById(event.target.id);
+        cell.innerHTML = cell.getAttribute("data");
+        cell.style.backgroundColor = "green";
+        score++;
+        scoreElement.innerHTML = score;
+  }
     
-})
+
+});
+
+//function showResult()
+//{
+//if( flagLeft == 0 || score >= 90)
+//   {
+//      resultElement.innerHTML = "YOU WIN!";
+//   }
+//    
+//}
+   
